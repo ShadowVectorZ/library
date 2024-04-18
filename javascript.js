e2d73c21448c4c14ba836c74c529c89a3d657ebf
ShadowVectorZ <rayaudet19@gmail.com>
2024-04-18 11:14:02 -0400

    removed test code

--- javascript.js
+++ javascript.js
@@ -18,17 +18,3 @@ console.log(myLibrary)
 }
 
 
-let data_array = [];
-
-// our object
-let my_object = {}; 
-   
-// load data into object
-
-my_object.name = "stack";
-my_object.age = 20;
-my_object.hair_color = "red";
-my_object.eye_color = "green";
-        
-// push the object to Array
-data_array.push(my_object);
