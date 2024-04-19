4691d4f916abdf3501c88c59cb1a30f58c265aee
ShadowVectorZ <rayaudet19@gmail.com>
2024-04-19 10:17:39 -0400

    added function that will display books (unfinished)

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const myLibrary=[];
 
+
+//object constructior
 function Book(title,author,pageNumber,read){
     this.title=title;
     this.author=author;
@@ -7,14 +9,27 @@ function Book(title,author,pageNumber,read){
     this.read=read;
 }
 
+let newBook
+//adds book to array
 function addBookToLibrary(){
  let title=(prompt("What is the title of your book?"))
  let author=(prompt("who is the author of your book?"))
 let pageNumber=(prompt("how many page numbers does it have?"))
 let read=(prompt("have you already read it?"));
-let newBook= new Book(title,author,pageNumber,read);
+ newBook= new Book(title,author,pageNumber,read);
 myLibrary.push(newBook)
 console.log(myLibrary)
 }
 
 
+const display=document.querySelector('#display');
+
+//supposed to print the properties of each book(object)
+function displayBooks(){
+    for(let i=0;i<myLibrary.length;i++){
+    let content=document.createElement('div')
+    content.classList.add('content');
+   content.textContent= /* don't know what to put here*/
+    display.appendChild(content)
+    }
+}
