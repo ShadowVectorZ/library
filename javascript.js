d484dba382547b3e95d836ac80dfbbe18fa3aa5d
ShadowVectorZ <rayaudet19@gmail.com>
2024-04-24 11:35:27 -0400

    added function that lets you delete books from display

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
 
-
-
-//object constructior
 function Book(title,author,pageNumber,read){
     this.title=title;
     this.author=author;
@@ -15,16 +12,25 @@ let book2=new Book('Dune', 'Frank Hebert', '650', 'unread');
 
 const myLibrary=[book1, book2];
 
-let newBook
-//adds book to array
+
 function addBookToLibrary(){
  let title=(prompt("What is the title of your book?"))
  let author=(prompt("who is the author of your book?"))
 let pageNumber=(prompt("how many page numbers does it have?"))
 let read=(prompt("have you already read it?"));
- newBook= new Book(title,author,pageNumber,read);
+let newBook= new Book(title,author,pageNumber,read);
 myLibrary.push(newBook)
-console.log(myLibrary)
+let content=document.createElement('div')
+content.classList.add('content');
+content.textContent= `Title: ${newBook.title}, Author: ${newBook.author}, Pages: ${newBook.pageNumber}, Read Status: ${newBook.read} `
+let delButton=document.createElement('button')
+delButton.classList.add('del-button')
+delButton.textContent='Delete'
+content.appendChild(delButton)
+ display.appendChild(content)
+ delButton.addEventListener('click', () => {
+     content.remove();
+    });
 }
 
 
@@ -32,13 +38,24 @@ const display=document.querySelector('#display');
 
 
 function displayBooks(){
+    
     for(let i=0;i<myLibrary.length;i++){
     let content=document.createElement('div')
     content.classList.add('content');
-    
    content.textContent= `Title: ${myLibrary[i].title}, Author: ${myLibrary[i].author}, Pages: ${myLibrary[i].pageNumber}, Read Status: ${myLibrary[i].read} `
+   let delButton=document.createElement('button')
+   delButton.classList.add('del-button')
+   delButton.textContent='Delete'
+   content.appendChild(delButton)
     display.appendChild(content)
+    delButton.addEventListener('click', () => {
+        content.remove();
+       });
     }
 }
 
 console.log(displayBooks())
+
+const addButton=document.querySelector('#new-book')
+addButton.addEventListener('click',addBookToLibrary)
+
