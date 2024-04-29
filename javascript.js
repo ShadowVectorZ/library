265db559e359ac2af9807b038aa8396094653b69
ShadowVectorZ <rayaudet19@gmail.com>
2024-04-29 10:56:11 -0400

    fixed bug where it kept duplicating books also allowed to cancel out of the dialog

--- javascript.js
+++ javascript.js
@@ -6,65 +6,62 @@ function Book(title,author,pageNumber,read){
     this.read=read;
 }
 
-let book1=new Book('The Hobbit', 'J.R.R. Tolkien', '255', 'read');
-let book2=new Book('Dune', 'Frank Hebert', '650', 'unread');
+let book1=new Book('The Hobbit', 'J.R.R. Tolkien', '255', 'Read');
+let book2=new Book('Dune', 'Frank Hebert', '650', 'Unread');
 
 const myLibrary=[book1, book2];
 
-
 const dialog=document.querySelector('dialog')
-const showButton=document.querySelector('dialog+button')
-const closeButton=document.querySelector('dialog button')
-
-showButton.addEventListener('click',()=>{
-    dialog.showModal()
-})
-
-closeButton.addEventListener('click',()=>{
-    dialog.close()
-})
+const addButton=document.querySelector('dialog+button')
+addButton.addEventListener('click',addBookToLibrary)
+const modalForm=document.querySelector('form')
+const cancel = document.querySelector("#cancel");
+cancel.addEventListener("click", () => {
+    dialog.close();
+});
 
 
-
-
-/*need to switch prompts with something else, put title author eyc in the function */
 function addBookToLibrary(){
- let title=(prompt("What is the title of your book?"))
- let author=(prompt("who is the author of your book?"))
-let pageNumber=(prompt("how many page numbers does it have?"))
-let read=(prompt("have you already read it?"));
-let newBook= new Book(title,author,pageNumber,read);
-myLibrary.push(newBook)
-let content=document.createElement('div')
-content.classList.add('content');
-content.textContent= `Title: ${newBook.title}, Author: ${newBook.author}, Pages: ${newBook.pageNumber}, Read Status: ${newBook.read} `
-let delButton=document.createElement('button')
-delButton.classList.add('del-button')
-delButton.textContent='Delete'
-content.appendChild(delButton)
-
-
-let statusButton=document.createElement('button')
-statusButton.classList.add('status-button')
-statusButton.textContent=`${newBook.read}`
-content.appendChild(statusButton)
+    dialog.showModal()
+    modalForm.addEventListener('submit',(event)=>{
+        event.preventDefault()
+ let title=document.querySelector('#book-title').value
+ let author=document.querySelector('#book-author').value
+ let pageNumber=document.querySelector('#page-number').value
+ let read=document.querySelector('select').value
+ if (pageNumber===undefined||pageNumber===null||pageNumber===''){
+    modalForm.reset()
+    dialog.close()}
+ else{
+ let newBook= new Book(title,author,pageNumber,read);
+ myLibrary.push(newBook)
+ let content=document.createElement('div')
+ content.classList.add('content');
+ content.textContent= `Title: ${newBook.title}, Author: ${newBook.author}, Pages: ${newBook.pageNumber} `
+ let delButton=document.createElement('button')
+ delButton.classList.add('del-button')
+ delButton.textContent='Delete'
+ content.appendChild(delButton)
+ let statusButton=document.createElement('button')
+ statusButton.classList.add('status-button')
+ statusButton.textContent=`${newBook.read}`
+ content.appendChild(statusButton)
  display.appendChild(content)
- 
  delButton.addEventListener('click', () => {
      content.remove();
     });
+ modalForm.reset()
+ dialog.close()
 }
-
+ })}
 
 const display=document.querySelector('#display');
 
-
 function displayBooks(){
-    
     for(let i=0;i<myLibrary.length;i++){
     let content=document.createElement('div')
     content.classList.add('content');
-   content.textContent= `Title: ${myLibrary[i].title}, Author: ${myLibrary[i].author}, Pages: ${myLibrary[i].pageNumber}, Read Status: ${myLibrary[i].read} `
+   content.textContent= `Title: ${myLibrary[i].title}, Author: ${myLibrary[i].author}, Pages: ${myLibrary[i].pageNumber}`
    delButton=document.createElement('button')
    delButton.classList.add('del-button')
    delButton.textContent='Delete'
@@ -76,22 +73,15 @@ content.appendChild(statusButton)
     display.appendChild(content)
     delButton.addEventListener('click', () => {
         content.remove();
-       });
-      
+       });      
     }
 }
-
 console.log(displayBooks())
-
-// const addButton=document.querySelector('#new-book')
-// addButton.addEventListener('click',addBookToLibrary)
-
 
 display.addEventListener("click", (event) => {
     if(event.target.innerText === 'read') {
       event.target.innerText='unread'
     }
-
     else if (event.target.innerText==='unread'){
         event.target.innerText='read'
     }
